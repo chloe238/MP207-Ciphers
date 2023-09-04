bf7a152fb2b3876295b102f9d455902962491cde
kellychl <kellychl@grinnell.edu>
2023-09-04 18:39:16 -0500

    2nd commite test

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -5,7 +5,7 @@ public class CaesarCipher{
   public static void main(String[] args) throws Exception{
     if(args[1] == "encode"){ //change to an actual string comparison
       //call encode with arg[2] param
-
+      //
     }
 
   }
