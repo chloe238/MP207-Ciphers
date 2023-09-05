96777162c2581a96d2543daa80d70c023318f1bc
chloe238 <chloegirl0121@gmail.com>
2023-09-05 12:39:22 -0500

    Constructed main methods

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -3,10 +3,25 @@ import java.io.PrintWriter;
 public class CaesarCipher{
 
   public static void main(String[] args) throws Exception{
-    if(args[1] == "encode"){ //change to an actual string comparison
-      //call encode with arg[2] param
-      //
+    PrintWriter pen = new PrintWriter(System.out, true);
+    if (args.length != 2){
+      pen.println("Incorrect number of parameters. Usage: [command] [message]");
+      System.exit(1);
     }
 
+    if(args[0].compareTo("encode") == 0){
+      //call encode with arg[1] param
+      //
+      pen.println("success");
+      pen.flush();
+      
+    } else if(args[0].compareTo("decode") == 0){
+      //call decode method
+
+    } else {
+      pen.println("Invaild command. Vaild options are encode and decode");
+      System.exit(1);
+    }
+    System.exit(0);
   }
 }
--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,5 +1,27 @@
 import java.io.PrintWriter;
 
 public class VigenereCipher {
-  
+    
+    public static void main(String[] args) throws Exception{
+        PrintWriter pen = new PrintWriter(System.out, true);
+        if (args.length != 2){
+          pen.println("Incorrect number of parameters. Usage: [command] [message]");
+          System.exit(1);
+        }
+    
+        if(args[0].compareTo("encode") == 0){
+          //call encode with arg[1] param
+          //
+          pen.println("success");
+          pen.flush();
+          
+        } else if(args[0].compareTo("decode") == 0){
+          //call decode method
+    
+        } else {
+          pen.println("Invaild command. Vaild options are encode and decode");
+          System.exit(1);
+        }
+        System.exit(0);
+      }
 }
